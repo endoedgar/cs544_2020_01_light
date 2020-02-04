d895b241d4268c9fd273276bd0c7399a7f435c50
Adeola Adeleke <aadeola@miu.edu>
2020-02-04 14:55:34 -0600

    Automatic redirect to profile page after login

---
+++ src/main/java/cs544_2020_01_light_attendanceproject/controller/DefaultController.java
@@ -0,0 +1,24 @@
+package cs544_2020_01_light_attendanceproject.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+@Secured({"ROLE_STUDENT", "ROLE_FACULTY", "ROLE_ADMIN"})
+public class DefaultController {
+
+	
+	@GetMapping
+	public void loadIndex(HttpServletResponse httpResponse) throws Exception {
+		 String currentUsername = SecurityContextHolder.getContext().getAuthentication().getName();
+		httpResponse.sendRedirect("/user/"+currentUsername);
+	}
+}
--- src/main/java/cs544_2020_01_light_attendanceproject/controller/UserController.java
+++ src/main/java/cs544_2020_01_light_attendanceproject/controller/UserController.java
@@ -1,24 +1,31 @@
 package cs544_2020_01_light_attendanceproject.controller;
 
-import cs544_2020_01_light_attendanceproject.domain.Timeslot;
-import cs544_2020_01_light_attendanceproject.domain.User;
-import cs544_2020_01_light_attendanceproject.exceptions.AdminsCannotDeleteThemselvesException;
-import cs544_2020_01_light_attendanceproject.exceptions.UserNotFoundException;
-import cs544_2020_01_light_attendanceproject.service.TimeSlotService;
-import cs544_2020_01_light_attendanceproject.service.UserService;
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import javax.validation.Valid;
-import java.net.URI;
+import cs544_2020_01_light_attendanceproject.domain.User;
+import cs544_2020_01_light_attendanceproject.exceptions.AdminsCannotDeleteThemselvesException;
+import cs544_2020_01_light_attendanceproject.exceptions.UserNotFoundException;
+import cs544_2020_01_light_attendanceproject.service.UserService;
 
 @RestController
-@Secured("ROLE_ADMIN")
 @RequestMapping("/user")
 public class UserController {
     private UserService userService;
@@ -28,6 +35,7 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Secured(value = {"ROLE_ADMIN"})
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Object> newUser(@RequestBody @Valid User user) {
@@ -41,16 +49,19 @@ public class UserController {
         return ResponseEntity.created(location).build();
     }
 
+    @Secured(value = {"ROLE_ADMIN"})
     @GetMapping("/")
     public Iterable<User> all() {
         return userService.listUsers();
     }
 
+    @Secured(value = {"ROLE_ADMIN","ROLE_FACULTY","ROLE_STUDENT"})
     @GetMapping("/{username}")
     public User one(@PathVariable String username) {
         return userService.findOneUser(username).orElseThrow(() -> new UserNotFoundException(username));
     }
 
+    @Secured(value = {"ROLE_ADMIN"})
     @DeleteMapping("/{username}")
     public void deleteUser(@PathVariable String username) {
         String currentUsername = SecurityContextHolder.getContext().getAuthentication().getName();
@@ -59,6 +70,7 @@ public class UserController {
         userService.deleteByUsername(username);
     }
 
+    @Secured(value = {"ROLE_ADMIN"})
     @PutMapping("/{username}")
     public User replaceUser(@RequestBody @Valid User newUser, @PathVariable String username) {
         return userService.replaceUser(newUser, username);
